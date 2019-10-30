a0958921196dedb0b145bf771e33d8cb084cf20f
Isaacg94 <7248zack@gmail.com>
2019-10-30 15:32:26 +0300

    create TwitterClient class to stream data from twitter

--- twitter_client.py
+++ twitter_client.py
@@ -1,1 +1,12 @@
 from tweepy import Cursor, API
+
+
+# twitter client
+class TwitterClient:
+    """
+    Class to get data from twitter
+    """
+    def __init__(self, twitter_user = None):
+        self.auth = TwitterAuthenticator().authenticate_twitter()
+        self.twitter_client = API(self.auth)
+        self.twitter_user = twitter_user
