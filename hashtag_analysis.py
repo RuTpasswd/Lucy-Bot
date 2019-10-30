4d2310f6e27f7d95c88d94b57d2cc3a801da582e
Titus Ouko <titusouko@gmail.com>
2019-10-30 15:23:44 +0300

    Add function on_data to fetch data frm twitter

--- hashtag_analysis.py
+++ hashtag_analysis.py
@@ -11,4 +11,15 @@ class TwitterListener(StreamListener):
     def __init__(self, fetched_tweets_filename):
         self.fetched_tweets_filename = fetched_tweets_filename
 
-    
+    # takes in streamed data
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, "a") as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print(f"Error on data {e}")
+        return True
+
+   
